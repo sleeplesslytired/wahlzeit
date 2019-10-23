cccf426d526421e70093d690f4adc86d45984a3b
Fabian Krueger <fabian.krueger@fau.de>
2019-10-23 17:45:26 +0200

    Coordinate: add attribute description

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -1,8 +1,15 @@
 public class Coordinate {
+
+	/**
+	 * all three values triangulate the coordiante
+	 */
 	public static final double x;	
 	public static final double y;
 	public static final double z;
 
+	/**
+	 *
+	 */
 	public Coordinate(double x, double y, double z) {
 		this.x = x;
 		this.y = y;
