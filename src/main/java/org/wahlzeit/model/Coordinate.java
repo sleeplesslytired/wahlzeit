15a14b34d1ecebae1ddb2ff31fea16bf3872235d
Fabian Krueger <fabian.krueger@fau.de>
2019-10-23 17:34:32 +0200

    Coordinate: add class constructor

--- src/main/java/org/wahlzeit/model/Coordinate.java
+++ src/main/java/org/wahlzeit/model/Coordinate.java
@@ -3,4 +3,9 @@ public class Coordinate {
 	public static final double y;
 	public static final double z;
 
+	public Coordinate(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
 }
