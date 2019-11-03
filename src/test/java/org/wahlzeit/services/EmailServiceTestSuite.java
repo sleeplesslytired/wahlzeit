cfabd9fa1a136a40909cd501d15621e3d1d58c0f
Fabian Krueger <fabian.krueger@fau.de>
2019-11-03 17:01:20 +0100

    EmailServiceTestSuite: implement class

---
+++ src/test/java/org/wahlzeit/services/EmailServiceTestSuite.java
@@ -0,0 +1,11 @@
+package org.wahlzeit.services;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+import org.wahlzeit.services.EmailAddressTest;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({EmailAddressTest.class})
+public class EmailServiceTestSuite {
+}
