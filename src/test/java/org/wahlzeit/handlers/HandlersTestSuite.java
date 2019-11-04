27aeffad6149e0715bd6384fb4c7acf3b966151f
Fabian Krueger <fabian.krueger@fau.de>
2019-11-04 10:05:48 +0100

    HandlersTestSuite: add and implement class

---
+++ src/test/java/org/wahlzeit/handlers/HandlersTestSuite.java
@@ -0,0 +1,12 @@
+package org.wahlzeit.handlers;
+
+import org.wahlzeit.handlers.*;
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TellFriendTest.class})
+public class HandlersTestSuite {
+
+}
