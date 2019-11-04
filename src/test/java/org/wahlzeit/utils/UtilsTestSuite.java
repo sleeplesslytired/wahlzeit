595a426bc44323e70dbd1c49adfd20f162194a23
Fabian Krueger <fabian.krueger@fau.de>
2019-11-04 10:32:57 +0100

    UtilsTestSuite: add and implement class

---
+++ src/test/java/org/wahlzeit/utils/UtilsTestSuite.java
@@ -0,0 +1,12 @@
+package org.wahlzeit.utils;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+import org.wahlzeit.utils.*;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({StringUtilTest.class,
+					 VersionTest.class})
+public class UtilsTestSuite {
+
+}
