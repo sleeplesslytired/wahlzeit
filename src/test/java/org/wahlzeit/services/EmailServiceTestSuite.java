be48ba539441bdda1dd6f9c258e81c6ab718f825
Fabian Krueger <fabian.krueger@fau.de>
2019-11-04 10:46:45 +0100

    EmailServiceTestSuite: use MailinTestSuite

--- src/test/java/org/wahlzeit/services/EmailServiceTestSuite.java
+++ src/test/java/org/wahlzeit/services/EmailServiceTestSuite.java
@@ -3,12 +3,12 @@ package org.wahlzeit.services;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 import org.wahlzeit.services.*;
-import org.wahlzeit.services.mailing.*;
+import org.wahlzeit.services.mailing.MailingTestSuite;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({EmailAddressTest.class,
-					 EmailServiceTest.class,
+					 MailingTestSuite.class,
 					 LogBuilderTest.class})
 public class EmailServiceTestSuite {
 }
