ce3b8d9d6313c473a7c6b0943843f05dd20df9d5
Fabian Krueger <fabian.krueger@fau.de>
2019-11-04 10:46:22 +0100

    MailingTestSuite: add and implement class

---
+++ src/test/java/org/wahlzeit/services/mailing/MailingTestSuite.java
@@ -0,0 +1,12 @@
+package org.wahlzeit.services.mailing;
+
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+import org.wahlzeit.services.mailing.*;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({EmailServiceTest.class})
+public class MailingTestSuite {
+
+}
