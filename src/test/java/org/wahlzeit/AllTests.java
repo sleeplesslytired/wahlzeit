fc7f3be13233856e92d9789b2f3d71d6ee0d83cc
Fabian Krueger <fabian.krueger@fau.de>
2019-11-04 10:07:15 +0100

    AllTests: add HandlersTestSuite

--- src/test/java/org/wahlzeit/AllTests.java
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -3,9 +3,11 @@ package org.wahlzeit;
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 import org.wahlzeit.services.*;
+import org.wahlzeit.handlers.*;
 
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({EmailServiceTestSuite.class})
+@Suite.SuiteClasses({EmailServiceTestSuite.class,
+					 HandlersTestSuite.class})
 public class AllTests {
 }
