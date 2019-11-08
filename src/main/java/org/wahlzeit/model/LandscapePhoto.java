1b16e8b249ce75b223150544673d6c844b25e21e
Fabian Krueger <fabian.krueger@fau.de>
2019-11-08 16:44:27 +0100

    LandscapePhoto: add attribute landscapeDescriptors

--- src/main/java/org/wahlzeit/model/LandscapePhoto.java
+++ src/main/java/org/wahlzeit/model/LandscapePhoto.java
@@ -2,9 +2,12 @@ package org.wahlzeit.model;
 
 import com.googlecode.objectify.annotation.Entity;
 import org.wahlzeit.model.Photo;
+import java.util.List;
+import java.util.LinkedList;
 
 @Entity
 public class LandscapePhoto extends Photo {
+	private List<String> landscapeDescriptors = new LinkedList<String>();
 
 	public LandscapePhoto() {
 		super();
