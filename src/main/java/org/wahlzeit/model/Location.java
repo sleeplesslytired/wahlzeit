48ed358bab5f41d07cbf8ac91c78a69e74bbdf93
Fabian Krueger <fabian.krueger@fau.de>
2019-11-08 21:19:51 +0100

    Loaction::Location: add country as class attribute

--- src/main/java/org/wahlzeit/model/Location.java
+++ src/main/java/org/wahlzeit/model/Location.java
@@ -3,9 +3,11 @@ package org.wahlzeit.model;
 
 public class Location {
 	public final Coordinate coordinate;
+	private final String country;
 
-	public Location(Coordinate coordinate) {
+	public Location(Coordinate coordinate, String country) {
 		this.coordinate = coordinate;
+		this.country = country;
 	}
 
 	public Coordinate getCoordinate() {
