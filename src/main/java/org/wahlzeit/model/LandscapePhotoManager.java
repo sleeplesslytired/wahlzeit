ee171575fa43a2c00d9e5bfce87d5a27f5e44edb
Fabian Krueger <fabian.krueger@fau.de>
2019-11-10 18:53:45 +0100

    LandscapePhotoManager: add photoCache with explicit type <PhotoId, LandscapePhoto>

--- src/main/java/org/wahlzeit/model/LandscapePhotoManager.java
+++ src/main/java/org/wahlzeit/model/LandscapePhotoManager.java
@@ -16,4 +16,9 @@ public class LandscapePhotoManager extends PhotoManager {
 
 	private static final Logger log = Logger.getLogger(LandscapePhotoManager.class.getName());
 
+	/**
+	 * In-memory cache for photos
+	 */
+	protected Map<PhotoId, LandscapePhoto> photoCache = new HashMap<PhotoId, LandscapePhoto>();
+
 }
