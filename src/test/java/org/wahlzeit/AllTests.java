b78f780d7ee6e5fd23f4a9ccb676de189933872a
Fabian Krueger <fabian.krueger@fau.de>
2019-11-10 12:30:14 +0100

    AllTests: specify imports

--- src/test/java/org/wahlzeit/AllTests.java
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -2,12 +2,11 @@ package org.wahlzeit;
 
 import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
-import org.wahlzeit.services.*;
-import org.wahlzeit.handlers.*;
+import org.wahlzeit.services.EmailServiceTestSuite;
+import org.wahlzeit.handlers.HandlersTestSuite;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({EmailServiceTestSuite.class,
-					 HandlersTestSuite.class})
 public class AllTests {
 }
