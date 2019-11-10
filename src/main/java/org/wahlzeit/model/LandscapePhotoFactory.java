862a3049a288a6c4cc0f2fb17c22bb75e3ce1c00
Fabian Krueger <fabian.krueger@fau.de>
2019-11-10 17:17:06 +0100

    LandscapePhotoFactory:: move from test to main tree

--- src/main/java/org/wahlzeit/model/LandscapePhotoFactory.java
+++ src/main/java/org/wahlzeit/model/LandscapePhotoFactory.java
@@ -1,8 +1,53 @@
 package org.wahlzeit.model;
 
-import org.wahlzeit.model.PhotoFactory;
+import org.wahlzeit.services.LogBuilder;
 
+import java.util.logging.Logger;
 
 public class LandscapePhotoFactory extends PhotoFactory {
+	private static final Logger log = Logger.getLogger(LandscapePhotoFactory.class.getName());
+	private static LandscapePhotoFactory instance = null;
 
+	protected LandscapePhotoFactory() {
+		super();
+	}
+
+	public static void initialize() {
+		getInstance();
+	}
+
+	public static synchronized LandscapePhotoFactory getInstance() {
+		if (this.instance == null) {
+			log.config(LogBuilder.createSystemMessage().addAction("setting generic LandscapePhotoFactory").toString());
+			setInstance(new LandscapePhotoFactory());
+		}
+
+		return instance;
+	}
+
+	protected static synchronized void setInstance(LandscapePhotoFactory landscapePhotoFactory ) {
+		if (instance != null) {
+			throw new IllegalStateException("attempt to initialize LandscapePhotoFactory twice");
+		}
+
+		instance = landscapePhotoFactory;
+	}
+
+	public LandscapePhoto createLandscapePhoto() {
+		return new LandscapePhoto();
+	}
+
+	public LandscapePhoto createLandscapePhoto(PhotoId id) {
+		return new LandscapePhoto(id);
+	}
+
+	public LandscapePhoto createLandscapePhoto(Location location) {
+		return new LandscapePhoto(location);
+	}
+
+	public LandscapePhoto loadLandscapePhoto(PhotoId id) {
+		return null;
+	}
+
+	
 }
