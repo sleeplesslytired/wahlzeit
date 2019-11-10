7d73dd1b9a9b10c896c2ac0bb7e021908b7c9106
Fabian Krueger <fabian.krueger@fau.de>
2019-11-10 17:11:19 +0100

    LandscapePhotoFactory: add class attributes

--- src/test/java/org/wahlzeit/model/LandscapePhotoFactory.java
+++ src/test/java/org/wahlzeit/model/LandscapePhotoFactory.java
@@ -5,4 +5,6 @@ import org.wahlzeit.services.LogBuilder;
 import java.util.logging.Logger;
 
 public class LandscapePhotoFactory extends PhotoFactory {
+	private static final Logger log = Logger.getLogger(LandscapePhotoFactory.class.getName());
+	private static LandscapePhotoFactory instance = null;
 }
