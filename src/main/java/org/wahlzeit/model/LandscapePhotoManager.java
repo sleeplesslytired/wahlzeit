20d510c3890f517d85ff6957212495637cfdaeaa
Fabian Krueger <fabian.krueger@fau.de>
2019-11-10 18:23:34 +0100

    LandscapePhotoManager: add class attributes

--- src/main/java/org/wahlzeit/model/LandscapePhotoManager.java
+++ src/main/java/org/wahlzeit/model/LandscapePhotoManager.java
@@ -2,7 +2,16 @@ package org.wahlzeit.model;
 
 import org.wahlzeit.model.PhotoManager;
 
+import java.util.logging.Logger;
+
 
 public class LandscapePhotoManager extends PhotoManager {
+
+	/**
+	 *
+	 */
+	protected static final LandscapePhotoManager instance = new LandscapePhotoManager();
+
+	private static final Logger log = Logger.getLogger(PhotoManager.class.getName());
 
 }
