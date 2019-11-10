cd7f7b2f5e3dd35ad8644ba5d0eaeba1c420b35a
Fabian Krueger <fabian.krueger@fau.de>
2019-11-10 12:30:29 +0100

    AllTests: also test ModelTestSuite

--- src/test/java/org/wahlzeit/AllTests.java
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -4,9 +4,12 @@ import org.junit.runners.Suite;
 import org.junit.runner.RunWith;
 import org.wahlzeit.services.EmailServiceTestSuite;
 import org.wahlzeit.handlers.HandlersTestSuite;
+import org.wahlzeit.model.ModelTestSuite;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({EmailServiceTestSuite.class,
+					 HandlersTestSuite.class,
+					 ModelTestSuite.class})
 public class AllTests {
 }
