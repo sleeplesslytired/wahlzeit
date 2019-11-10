bfa34ed9e1a17d4a810c65b916d95b52de52d6dc
Fabian Krueger <fabian.krueger@fau.de>
2019-11-10 17:30:23 +0100

    LandscapePhotoFactory: add function descriptions

--- src/main/java/org/wahlzeit/model/LandscapePhotoFactory.java
+++ src/main/java/org/wahlzeit/model/LandscapePhotoFactory.java
@@ -6,16 +6,28 @@ import java.util.logging.Logger;
 
 public class LandscapePhotoFactory extends PhotoFactory {
 	private static final Logger log = Logger.getLogger(LandscapePhotoFactory.class.getName());
+	/**
+	 * Hidden singleton instance; needs to be initialized from the outside.
+	 */
 	private static LandscapePhotoFactory instance = null;
 
+	/**
+	 *
+	 */
 	protected LandscapePhotoFactory() {
 		super();
 	}
 
+	/**
+	 * Hidden singleton instance; needs to be initialized from the outside.
+	 */
 	public static void initialize() {
 		getInstance();
 	}
 
+	/**
+	 * Public singleton access method.
+	 */
 	public static synchronized LandscapePhotoFactory getInstance() {
 		if (this.instance == null) {
 			log.config(LogBuilder.createSystemMessage().addAction("setting generic LandscapePhotoFactory").toString());
@@ -25,6 +37,9 @@ public class LandscapePhotoFactory extends PhotoFactory {
 		return instance;
 	}
 
+	/**
+	 * Method to set the singleton instance of PhotoFactory.
+	 */
 	protected static synchronized void setInstance(LandscapePhotoFactory landscapePhotoFactory ) {
 		if (instance != null) {
 			throw new IllegalStateException("attempt to initialize LandscapePhotoFactory twice");
@@ -33,18 +48,31 @@ public class LandscapePhotoFactory extends PhotoFactory {
 		instance = landscapePhotoFactory;
 	}
 
+	/**
+	 * @methodtype factory
+	 */
 	public LandscapePhoto createLandscapePhoto() {
 		return new LandscapePhoto();
 	}
 
+	/**
+	 * Creates a new photo with the specified id
+	 */
 	public LandscapePhoto createLandscapePhoto(PhotoId id) {
 		return new LandscapePhoto(id);
 	}
 
+	/**
+	 * Creates a new photo with the specified location
+	 */
 	public LandscapePhoto createLandscapePhoto(Location location) {
 		return new LandscapePhoto(location);
 	}
 
+	/**
+	 * Loads a photo. The Java object is loaded from the Google Datastore, the Images in all sizes are loaded from the
+	 * Google Cloud storage.
+	 */
 	public LandscapePhoto loadLandscapePhoto(PhotoId id) {
 		return null;
 	}
