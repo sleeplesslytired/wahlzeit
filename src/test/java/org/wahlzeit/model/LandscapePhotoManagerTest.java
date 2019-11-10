71120abcdf32e6a40a403ccf74fee029e7d30aac
Fabian Krueger <fabian.krueger@fau.de>
2019-11-10 19:04:33 +0100

    LandscapePhotoManagerTest: add class stump

---
+++ src/test/java/org/wahlzeit/model/LandscapePhotoManagerTest.java
@@ -0,0 +1,13 @@
+package org.wahlzeit.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.wahlzeit.services.EmailAddress;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class LandscapePhotoManagerTest {
+
+}
