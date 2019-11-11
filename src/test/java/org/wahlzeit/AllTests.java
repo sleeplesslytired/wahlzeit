8a74c673e9f5788d4d23ec6325886f9ad7bff19f
Fabian Krueger <fabian.krueger@fau.de>
2019-11-11 10:36:57 +0100

    AllTests: add TestEnvironmentProviderTestSuite, UtilsTestSuite

--- src/test/java/org/wahlzeit/AllTests.java
+++ src/test/java/org/wahlzeit/AllTests.java
@@ -5,11 +5,15 @@ import org.junit.runner.RunWith;
 import org.wahlzeit.services.EmailServiceTestSuite;
 import org.wahlzeit.handlers.HandlersTestSuite;
 import org.wahlzeit.model.ModelTestSuite;
+import org.wahlzeit.testEnvironmentProvider.TestEnvironmentProviderTestSuite;
+import org.wahlzeit.utils.UtilsTestSuite;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({EmailServiceTestSuite.class,
 					 HandlersTestSuite.class,
-					 ModelTestSuite.class})
+					 ModelTestSuite.class,
+					 TestEnvironmentProviderTestSuite.class,
+					 UtilsTestSuite.class})
 public class AllTests {
 }
