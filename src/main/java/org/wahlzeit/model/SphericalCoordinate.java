c37d696d003f2bc8df46c66b9747267fe7b18463
Fabian Krueger <fabian.krueger@fau.de>
2019-11-16 14:51:28 +0100

    SphericalCoordinate::SphericalCoordinate: implement constructor

--- src/main/java/org/wahlzeit/model/SphericalCoordinate.java
+++ src/main/java/org/wahlzeit/model/SphericalCoordinate.java
@@ -1,6 +1,39 @@
 package org.wahlzeit.model;
 
 
-public class SphericalCoordinate {
+public class SphericalCoordinate implements Coordinate {
+
+	/**
+	 * all three spherical coordinates
+	 */
+	private final double theta;
+	private final double phi;
+	private final double r;
+
+	public SphericalCoordinate(double theta, double phi, double r) {
+		this.theta = theta;
+		this.phi = phi;
+		this.r = r;
+	}
+
+	public CartesianCoordinate asCartesian() {
+		return null;
+	}
+
+	public double getCartesianDistance(Coordinate coordinate) {
+		return -1;
+	}
+
+	public SphericalCoordinate asSpherical() {
+		return null;
+	}
+
+	public double getSphericalDistance(Coordinate coordinate) {
+		return -1;
+	}
+
+	public boolean isEqual(Coordinate coordinate) {
+		return false;
+	}
 
 }
