8cc4a89fb3a55cd659b8930052313da01440bbed
Fabian Krueger <fabian.krueger@fau.de>
2019-11-16 17:07:19 +0100

    CartesianCoordinateTest::setup: add and implement method

--- src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
@@ -8,5 +8,16 @@ import static org.junit.Assert.assertTrue;
 
 
 public class CartesianCoordinateTest {
+	
+	protected cartesianCoordinate0;
+	protected cartesianCoordinate1;
+	protected sphericalCoordinate;
+
+	@Before
+	public void setup() {
+		cartesianCoordinate0 = new CartesianCoordinate(10., -5., 0.);
+		cartesianCoordinate1 = new CartesianCoordinate(-5., -10., 0.5);
+		sphericalCoordinate = new SphericalCoordinate(45., 0., 36.);
+	}
 
 }
