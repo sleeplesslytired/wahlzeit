af61a7a15667ba1c63b7100d0aed95d04ade125a
Fabian Krueger <fabian.krueger@fau.de>
2019-11-16 17:43:23 +0100

    CartesianCoordinatesTest: add tests for cartesian distance

--- src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
+++ src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
@@ -7,17 +7,37 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
+import java.lang.Math;
+
+
 public class CartesianCoordinateTest {
 	
-	protected cartesianCoordinate0;
-	protected cartesianCoordinate1;
-	protected sphericalCoordinate;
+	protected CartesianCoordinate cartesianCoordinate0;
+	protected CartesianCoordinate cartesianCoordinate1;
+	protected SphericalCoordinate sphericalCoordinate;
+	protected double epsilon;
 
 	@Before
 	public void setup() {
 		cartesianCoordinate0 = new CartesianCoordinate(10., -5., 0.);
 		cartesianCoordinate1 = new CartesianCoordinate(-5., -10., 0.5);
 		sphericalCoordinate = new SphericalCoordinate(45., 0., 36.);
+		epsilon = 1;
+	}
+
+	@Test
+	public void testCartesianDistanceOtherCartesian0() {
+		assertTrue(Math.abs(cartesianCoordinate0.getCartesianDistance(cartesianCoordinate1) - 15.) < epsilon);
+	}
+
+	@Test
+	public void testCartesianDistanceOtherCartesian1() {
+		assertTrue(Math.abs(cartesianCoordinate1.getCartesianDistance(cartesianCoordinate0) - 15.) < epsilon);
+	}
+
+	@Test
+	public void testCartesianDistanceSpherical() {
+		assertTrue(Math.abs(cartesianCoordinate0.getCartesianDistance(sphericalCoordinate) - 28) < epsilon);
 	}
 
 }
