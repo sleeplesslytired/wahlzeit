dc12bf3dfd1dc7bfb3e06fc7f1b1cfb8be463a59
Fabian Krueger <fabian.krueger@fau.de>
2019-11-16 17:04:16 +0100

    CartesianCoordinateTest: add class stump

---
+++ src/test/java/org/wahlzeit/model/CartesianCoordinateTest.java
@@ -0,0 +1,12 @@
+package org.wahlzeit.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+
+public class CartesianCoordinateTest {
+
+}
