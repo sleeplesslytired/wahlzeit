a4b2d752149a21e34c7c72196442a8e1c963c443
Fabian Krueger <fabian.krueger@fau.de>
2019-12-12 10:49:41 +0100

    CartesianCoordinateHandler: add implementation class

---
+++ src/main/java/org/wahlzeit/model/CartesianCoordinateHandler.java
@@ -0,0 +1,11 @@
+package org.wahlzeit.model;
+
+
+public class CartesianCoordinateHandler extends AbstractCoordinateHandler {
+	public void setCoordinate(Coordinate coordinate) {
+	}
+
+	public Coordinate getCoordinate() {
+		return null;
+	}
+}
