86234f8581bf4d0bb21d8c000d9fc3a0566adb76
Fabian Krueger <fabian.krueger@fau.de>
2019-12-12 10:51:23 +0100

    CartesianCoordinateHandler::setCoordinate: implement

--- src/main/java/org/wahlzeit/model/CartesianCoordinateHandler.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinateHandler.java
@@ -3,6 +3,7 @@ package org.wahlzeit.model;
 
 public class CartesianCoordinateHandler extends AbstractCoordinateHandler {
 	public void setCoordinate(Coordinate coordinate) {
+		this.coordinateBody = coordinate.asCartesian();
 	}
 
 	public Coordinate getCoordinate() {
