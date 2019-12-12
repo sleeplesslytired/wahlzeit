e3be040fe337a2067257a8eb30b3465a9bc7f04f
Fabian Krueger <fabian.krueger@fau.de>
2019-12-12 10:48:43 +0100

    SphericalCoordinateHandler: add implementation class

---
+++ src/main/java/org/wahlzeit/model/SphericalCoordinateHandler.java
@@ -0,0 +1,12 @@
+package org.wahlzeit.model;
+
+
+public class SphericalCoordinateHandler extends AbstractCoordinateHandler {
+	public void setCoordinate(Coordinate coordinate) {
+	}
+
+	public Coordinate getCoordinate() {
+		return null;
+	}
+
+}
