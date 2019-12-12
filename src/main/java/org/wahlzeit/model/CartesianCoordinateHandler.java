e017d48d171c34e6a0957487c82d724d47c3aa0d
Fabian Krueger <fabian.krueger@fau.de>
2019-12-12 10:52:37 +0100

    CartesianCoordinateHandler: constructor: implement

--- src/main/java/org/wahlzeit/model/CartesianCoordinateHandler.java
+++ src/main/java/org/wahlzeit/model/CartesianCoordinateHandler.java
@@ -2,6 +2,10 @@ package org.wahlzeit.model;
 
 
 public class CartesianCoordinateHandler extends AbstractCoordinateHandler {
+	public CartesianCoordinateHandler(Coordinate coordinate) {
+		this.coordinateBody = coordinate.asCartesian();
+	}
+
 	public void setCoordinate(Coordinate coordinate) {
 		this.coordinateBody = coordinate.asCartesian();
 	}
