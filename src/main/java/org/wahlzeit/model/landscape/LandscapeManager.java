a35e4dda4ec0671f432f3e30ebfcd9f8193644a9
Fabian Krueger <fabian.krueger@fau.de>
2020-01-11 14:48:54 +0100

    LandscapeManager: add class

---
+++ src/main/java/org/wahlzeit/model/landscape/LandscapeManager.java
@@ -0,0 +1,13 @@
+package org.wahlzeit.model.landscape;
+
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import org.wahlzeit.model.*;
+
+
+public class LandscapeManager {
+
+}
