fa172838093654b5a7053934c44e5fe6e21439e1
Fabian Krueger <fabian.krueger@fau.de>
2020-01-11 14:55:35 +0100

    LandscapeManager::getInstance: add

--- src/main/java/org/wahlzeit/model/landscape/LandscapeManager.java
+++ src/main/java/org/wahlzeit/model/landscape/LandscapeManager.java
@@ -9,5 +9,11 @@ import org.wahlzeit.model.*;
 
 
 public class LandscapeManager {
+	protected static LandscapeManager instance = new LandscapeManager();	
+	protected static Set<LandscapeType> landscapeTypes = new HashSet<LandscapeType>();
+
+	public static final LandscapeManager getInstance() {
+		return instance;
+	}
 
 }
