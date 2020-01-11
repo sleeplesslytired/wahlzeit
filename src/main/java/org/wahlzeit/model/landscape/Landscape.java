ca712f5f10cb4204940f6bc740cffdb1fd55a2fc
Fabian Krueger <fabian.krueger@fau.de>
2020-01-11 14:54:17 +0100

    Landscape: add creators

--- src/main/java/org/wahlzeit/model/landscape/Landscape.java
+++ src/main/java/org/wahlzeit/model/landscape/Landscape.java
@@ -5,5 +5,16 @@ import org.wahlzeit.model.*;
 
 
 public class Landscape {
+	private Location location;
+	private LandscapeType landscapeType;
+
+	public Landscape(Location location) {
+		this.location = location;
+	}
+
+	public Landscape(Location location, LandscapeType landscapeType) {
+		this.location = location;
+		this.landscapeType = landscapeType;
+	}
 
 }
