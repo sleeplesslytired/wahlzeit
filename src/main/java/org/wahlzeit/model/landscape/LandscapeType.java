3a185be93ebfdcaa7ecc420601577eac8c836c13
Fabian Krueger <fabian.krueger@fau.de>
2020-01-11 14:51:53 +0100

    LandscapeType: add getter and setter for characteristics

--- src/main/java/org/wahlzeit/model/landscape/LandscapeType.java
+++ src/main/java/org/wahlzeit/model/landscape/LandscapeType.java
@@ -21,4 +21,26 @@ public class LandscapeType {
 
 	}
 
+	public void addCharacteristic(String characteristic) {
+		if (characteristic == null) {
+			throw new IllegalArgumentException("can not add null as characteristic to a landscape type!");
+		}
+
+		this.characteristics.add(characteristic);
+	}
+
+	public void addCharacteristics(Iterator<String> characteristics) {
+		if (characteristics == null) {
+			throw new IllegalArgumentException("can not add characteristics from a null iterator to a landscape type!");
+		}
+
+		while (characteristics.hasNext()) {
+			this.addCharacteristic(characteristics.next());
+		}
+	}
+
+	public Iterator<String> getCharacteristicsIterator() {
+		return this.characteristics.iterator();
+	}
+
 }
