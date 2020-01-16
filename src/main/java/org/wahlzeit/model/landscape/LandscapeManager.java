ed8424cda3d9c838eaf96036b887d1df625a3252
Fabian Krueger <fabian.krueger@fau.de>
2020-01-16 15:28:06 +0100

    LandscapeManager::createLandscape: allow insantiation of a landscape with type name

--- src/main/java/org/wahlzeit/model/landscape/LandscapeManager.java
+++ src/main/java/org/wahlzeit/model/landscape/LandscapeManager.java
@@ -18,12 +18,35 @@ public class LandscapeManager {
 
 	public Landscape createLandscape(Location location, LandscapeType landscapeType) {
 		if (!this.landscapeTypes.contains(landscapeType)) {
-			throw new IllegalArgumentException("No matching landscape type found to characteristics");
+			// throw new IllegalArgumentException("No matching landscape type found to characteristics");
+			this.landscapeTypes.add(landscapeType);
 		} 
 
 		Landscape landscape = landscapeType.createInstance(location);
 
 		return landscape;
+	}
+
+	public Landscape createLandscape(Location location, String landscapeName) {
+		LandscapeType landscapeType = this.getLandscapeTypeByName(landscapeName);
+		if (!this.landscapeTypes.contains(landscapeType)) {
+			throw new IllegalArgumentException("No matching landscape type found to characteristics");
+		} 
+
+		return this.createLandscape(location, landscapeType);
+	}
+
+	protected LandscapeType getLandscapeTypeByName(String name) {
+		Iterator<LandscapeType> types = this.landscapeTypes.iterator();
+		
+		while(types.hasNext()) {
+			LandscapeType type = types.next();
+			if (type.getName().equals(name)) {
+				return type;
+			}
+		}
+
+		return null;
 	}
 
 	public void addLandscapeType(LandscapeType landscapeType) {
